90ef7e77ea965067cd27370a142699e87b32a335
kreison <alankainazovr@gmail.com>
2023-07-14 15:01:30 +0600

    update config

--- src/configs/firebase/firebase.js
+++ src/configs/firebase/firebase.js
@@ -1,13 +1,12 @@
 import firebase from 'firebase';
  
 const firebaseConfig = {
-  apiKey: "AIzaSyAggbxjxZ8vhGyEasEzyCJulue5txNSUUQ",  
-  authDomain: "otp-phone-ea164.firebaseapp.com",
-  projectId: "otp-phone-ea164",
-  storageBucket: "otp-phone-ea164.appspot.com",
-  messagingSenderId: "920556560008",
-  appId: "1:920556560008:web:852025741f1a7b103e093d"
-
+  apiKey: "AIzaSyBLynClttewzjxJBtzDORREzXi8S0j8pmE",
+  authDomain: "new-otp-phone.firebaseapp.com",
+  projectId: "new-otp-phone",
+  storageBucket: "new-otp-phone.appspot.com",
+  messagingSenderId: "16879116106",
+  appId: "1:16879116106:web:8cdca733e7285a44e22bb3"
 }
 
 firebase.initializeApp(firebaseConfig);
